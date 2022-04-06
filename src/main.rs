c8631197837e34d98ebdae5996064c19336ce2f7
Shahryar Soltanpour <shahryar.soltanpour@gmail.com>
2022-04-06 11:12:01 -0600

    Implement csv parser

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+mod read_csv;
+
 use warp::{ws::WebSocket, Filter, Rejection, Reply};
 
 #[tokio::main]
 async fn main() {
+    let records = read_csv::parse("src/dailyActivity_merged.csv")
+        .expect("Parse csv file");
+
     let metrics_route = warp::path!("metrics").and_then(metrics_handler);
 
     println!("Started on port 8080");
---
+++ src/read_csv.rs
@@ -0,0 +1,35 @@
+use std::error::Error;
+use std::fs::File;
+use std::io;
+use std::process;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Record {
+    ActivityDate: String,
+    TotalSteps: u32,
+    TotalDistance: f32,
+    Calories: u32,
+}
+
+pub fn parse(file_path: &str) -> Result<(Vec<Record>), Box<dyn Error>> {
+    // Build the CSV reader and iterate over each record.
+    // let file_path = "src/dailyActivity_merged.csv";
+
+    let file = File::open(file_path)?;
+    let mut rdr = csv::Reader::from_reader(file);
+    let mut result = Vec::new();
+    let x = rdr.deserialize::<Record>();
+    for row in rdr.deserialize::<Record>() {
+        let record = row?;
+        // println!("{}", record.Miles);
+        result.push(record);
+        // track_fitness_data(
+        //     &record.ActivityDate.to_string(),
+        //     &record.TotalSteps.to_string(),
+        //     &record.TotalDistance.to_string(),
+        //     &record.Calories.to_string(),
+        // )
+    }
+    Ok(result)
+}
