f0f6d93eba9effbaf753d6c21c9b0f83fb6c288d
Shahryar Soltanpour <shahryar.soltanpour@gmail.com>
2022-04-06 10:25:41 -0600

    Implement a basic web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use warp::{ws::WebSocket, Filter, Rejection, Reply};
+
+#[tokio::main]
+async fn main() {
+    let metrics_route = warp::path!("metrics").and_then(metrics_handler);
+
+    println!("Started on port 8080");
+    warp::serve(metrics_route)
+        .run(([0, 0, 0, 0], 8081))
+        .await;
+}
+
+async fn metrics_handler() -> Result<impl Reply, Rejection> {
+    Ok("hello!")
 }
